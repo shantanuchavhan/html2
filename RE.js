d7b9f7d5053877e49399095a3df19b9da62b77d3
shantanuchavhan <97030984+shantanuchavhan@users.noreply.github.com>
2023-05-10 22:45:25 +0530

    Add files via upload

---
+++ RE.js
@@ -0,0 +1,78 @@
+ function a() {
+    document.getElementById("imagesection").src="./images/imagesectio/image 2.avif"
+ }
+ function b() {
+    document.getElementById("imagesection").src="./images/imagesectio/image 3.avif"
+ }
+ function c() {
+    document.getElementById("imagesection").src="./images/imagesectio/image 4.avif"
+ }
+ function d() {
+    document.getElementById("imagesection").src="./images/imagesectio/image 5.avif"
+ }
+ function e() {
+    document.getElementById("imagesection").src="./images/imagesectio/image 6.avif"
+ }
+ function f() {
+    document.getElementById("imagesection").src="./images/imagesectio/image 7.avif"
+ }
+ function g() {
+    document.getElementById("imagesection").src="./images/imagesectio/image 8.avif"
+ }
+ function h() {
+    document.getElementById("imagesection").src="./images/imagesectio/image 9.avif"
+ }
+
+
+
+
+function ride() {
+   document.getElementById("imagesection2").src="./images/imagesection2/royal-enfield-trail-school.avif"
+}
+
+ function ridea() {
+   document.getElementById("imagesection2").src="./images/imagesection2/image2.avif"
+}
+function rideb() {
+   document.getElementById("imagesection2").src="./images/imagesection2/image3.avif"
+}
+function ridec() {
+   document.getElementById("imagesection2").src="./images/imagesection2/image4.avif"
+}
+function rided() {
+   document.getElementById("imagesection2").src="./images/imagesection2/image5.avif"
+}
+
+function Apparel() {
+   document.getElementById("imagesection3").src="./images/imagesection3/image1.avif"
+}
+function Apparela() {
+   document.getElementById("imagesection3").src="./images/imagesection3/image2.avif"
+}
+function Apparelb() {
+   document.getElementById("imagesection3").src="./images/imagesection3/image3.avif"
+}
+function Apparelc() {
+   document.getElementById("imagesection3").src="./images/imagesection3/image4.avif"
+}
+function Appareld() {
+   document.getElementById("imagesection3").src="./images/imagesection3/image5.avif"
+}
+
+
+function Accesseries(){
+   document.getElementById("imagesection4").src="./images/imagesection4/gma-home.avif"
+}
+function Accesseriesa(){
+   document.getElementById("imagesection4").src="./images/imagesection4/image.avif"
+}
+
+
+
+
+function Locatea(){
+   document.getElementById("imagesection5").src="./images/imagesection5/image2.avif"
+}
+function Locatea(){
+   document.getElementById("imagesection5").src="./images/imagesection5/image3.avif"
+}
